13b10b3a5e3053ed461ed949d7a8b770a0e6a3f9
Rebecca Mpi <beckynmpi@gmail.com>
2022-10-29 21:00:00 +0100

    Updated with component hierarchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'; 
+import Form from './Form/form';
+import memories from './images/mainmemories.png'; 
+import Posts from './Posts/posts';
+import useStyles from './styles.js'; 
+
 
 function App() {
+  const classes = useStyles(); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" align="right" height="60" /> 
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+            <Posts/>
+            </Grid>
+
+             <Grid item xs={12} sm={4}>
+            <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+
   );
 }
 
---
+++ client/src/Form/form.js
@@ -0,0 +1,14 @@
+import useStyles from './styles.js'; 
+
+const Form = () => { 
+    const classes = useStyles(); 
+    
+    return (
+        <h1>
+            Forms
+        </h1>
+    )
+
+}
+ 
+export default Form; 
